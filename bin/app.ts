a2e6bdab80e20757789445c8abdaa7b700f180e9
Lee Priest <lee@leepriest.dev>
2025-05-20 23:55:28 +0100

    Naming fix

--- bin/app.ts
+++ bin/app.ts
@@ -3,7 +3,7 @@ import * as cdk from "aws-cdk-lib";
 import { ServiceCognitoStack } from "../cdk/stacks/ServiceCognitoStack";
 
 const stage = process.env.STAGE || "dev";
-const eventBusName = process.env.EVENT_BUS_NAME || `event-bus-${stage}`;
+const eventBusName = process.env.EVENT_BUS_NAME || "event-bus";
 
 const app = new cdk.App();
 new ServiceCognitoStack(app, "ServiceCognitoStack", {
